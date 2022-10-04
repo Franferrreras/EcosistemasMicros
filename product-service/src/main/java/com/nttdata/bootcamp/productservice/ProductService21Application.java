1d04e370ceb97e03c5d171b39208fedbe5e30479
Fran Ferreras <franferreraszl@gmail.com>
2022-10-04 21:21:05 +0200

    Config data eureka-service

--- product-service/src/main/java/com/nttdata/bootcamp/productservice/ProductService21Application.java
+++ product-service/src/main/java/com/nttdata/bootcamp/productservice/ProductService21Application.java
@@ -1,13 +1,9 @@
 package com.nttdata.bootcamp.productservice;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableEurekaClient
-@EnableAutoConfiguration
 public class ProductService21Application {
 
 	public static void main(String[] args) {
